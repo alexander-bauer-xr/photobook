7db039310585d4953d1495a2812d5e65f705eef4
Alexander Bauer <hi@blended-sahpes.com>
2026-05-10 12:51:04 +0200

    Fresh refactor

--- resources/photobook-editor/src/hooks/usePages.ts
+++
@@ -1,3 +0,0 @@
-// Backward-compatible alias to the consolidated photobook hook
-// This ensures components importing { usePages } get the stateful version with overrides hydration.
-export { usePhotobook as usePages } from './usePhotobook';
--- resources/views/photobook/editor.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-  <div id="photobook-root" class="min-h-screen bg-neutral-50"></div>
-@endsection
-
-@push('scripts')
-  @vite('resources/photobook-editor/main.tsx')
-@endpush
